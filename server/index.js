6b96de1f67613cac7acedaa12e947faa35ee4483
Danny McKinnon <dannymckinnon.dm@gmail.com>
2025-01-27 15:13:59 -0800

    removed localhost address from cors

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-app.use(cors({ origin: ['http://localhost:5173'] }));
+app.use(cors());
 
 app.get('/api', (req, res) => {
   res.json({ fruits: ['apple', 'asdfasdfsad', 'banana'] });
