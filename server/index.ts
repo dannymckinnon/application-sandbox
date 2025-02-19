c76202ddca87c43ef9bc1f8c311c2e5126901d78
Danny McKinnon <dannymckinnon.dm@gmail.com>
2025-02-18 17:03:05 -0800

    Uninstall nodemon and ts-node in favor of tsx

--- server/index.ts
+++ server/index.ts
@@ -1,23 +1,14 @@
 import express from 'express';
 import cors from 'cors';
+import { PORT, corsOptions } from './utils/config.ts';
 
 const app = express();
-
-const isProduction = process.env.NODE_ENV === 'production';
-const corsOptions = {
-  origin: isProduction
-    ? 'https://application-sandbox-client.onrender.com'
-    : 'http://localhost:5173',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-};
 
 app.use(cors(corsOptions));
 
 app.get('/api', (req: express.Request, res: express.Response) => {
   res.json({ fruits: ['apple', 'asdfasdfsad', 'banana'] });
 });
-
-const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
